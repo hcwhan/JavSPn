4e4870164b755e5f8a00a8196b59d514f0ef6910
glyh <lyhokia@gmail.com>
2024-04-17 19:59:24 +0800

    remove temporary codes

--- core/ai_crop/pphumanseg.py
+++ core/ai_crop/pphumanseg.py
@@ -1,5 +1,5 @@
 # This file is part of OpenCV Zoo project.
-# It is subject to the license terms in the LICENSE file found in the same directory.
+# It is subject to the license terms in the pphumanseg.LICENSE file found in the same directory.
 #
 # Copyright (C) 2021, Shenzhen Institute of Artificial Intelligence and Robotics for Society, all rights reserved.
 # Third party copyrights are property of their respective owners.
@@ -7,7 +7,7 @@
 import numpy as np
 import cv2 as cv
 
-# from core.config import cfg
+from core.config import cfg
 
 class PPHumanSeg:
     def __init__(self, modelPath, backendId=0, targetId=0):
@@ -70,13 +70,12 @@ class PPHumanSeg:
         result = np.argmax(outputBlob, axis=1).astype(np.uint8)
         return result
 
-# piccfg = cfg.Picture
-# if piccfg == None:
-#     raise Exception("Missing `Picture` field from config.ini")
-# model_path = piccfg.pphumanseg_model
-# if model_path == None:
-#     raise Exception("Missing `pphumanseg_model` for ai crop backend pphumanseg")
-model_path = "/home/lyh/Downloads/human_segmentation_pphumanseg_2023mar.onnx"
+piccfg = cfg.Picture
+if piccfg == None:
+    raise Exception("Missing `Picture` field from config.ini")
+model_path = piccfg.pphumanseg_model
+if model_path == None:
+    raise Exception("Missing `pphumanseg_model` for ai crop backend pphumanseg")
 
 # Valid combinations of backends and targets
 backend_target_pairs = [
@@ -90,62 +89,6 @@ backend_target_pairs = [
 backend_target = 0
 backend_id = backend_target_pairs[backend_target][0]
 target_id = backend_target_pairs[backend_target][1]
-
-# def get_color_map_list(num_classes):
-#     """
-#     Returns the color map for visualizing the segmentation mask,
-#     which can support arbitrary number of classes.
-#
-#     Args:
-#         num_classes (int): Number of classes.
-#
-#     Returns:
-#         (list). The color map.
-#     """
-#
-#     num_classes += 1
-#     color_map = num_classes * [0, 0, 0]
-#     for i in range(0, num_classes):
-#         j = 0
-#         lab = i
-#         while lab:
-#             color_map[i * 3] |= (((lab >> 0) & 1) << (7 - j))
-#             color_map[i * 3 + 1] |= (((lab >> 1) & 1) << (7 - j))
-#             color_map[i * 3 + 2] |= (((lab >> 2) & 1) << (7 - j))
-#             j += 1
-#             lab >>= 3
-#     color_map = color_map[3:]
-#     return color_map
-#
-# def visualize(image, result, weight=0.6, fps=None):
-#     """
-#     Convert predict result to color image, and save added image.
-#
-#     Args:
-#         image (str): The input image.
-#         result (np.ndarray): The predict result of image.
-#         weight (float): The image weight of visual image, and the result weight is (1 - weight). Default: 0.6
-#         fps (str): The FPS to be drawn on the input image.
-#
-#     Returns:
-#         vis_result (np.ndarray): The visualized result.
-#     """
-#     color_map = get_color_map_list(256)
-#     color_map = np.array(color_map).reshape(256, 3).astype(np.uint8)
-#
-#     # Use OpenCV LUT for color mapping
-#     c1 = cv.LUT(result, color_map[:, 0])
-#     c2 = cv.LUT(result, color_map[:, 1])
-#     c3 = cv.LUT(result, color_map[:, 2])
-#     pseudo_img = np.dstack((c1, c2, c3))
-#
-#     vis_result = cv.addWeighted(image, weight, pseudo_img, 1 - weight, 0)
-#
-#     if fps is not None:
-#         cv.putText(vis_result, 'FPS: {:.2f}'.format(fps), (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-#
-#     return vis_result
-
 
 model = PPHumanSeg(modelPath=model_path, backendId=backend_id, targetId=target_id)
 
@@ -169,7 +112,3 @@ def ai_crop_poster(fanart, poster='', hw_ratio=1.42):
     image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
     cropped = image[0:poster_h, poster_left:int(poster_left+poster_w)]
     cv.imwrite(poster, cropped) 
-
-if __name__ == "__main__":
-    import sys
-    ai_crop_poster(sys.argv[1])

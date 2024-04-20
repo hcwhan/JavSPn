ff40a129a42a0f5628cce7faf576b15e41e99f72
glyh <lyhokia@gmail.com>
2024-04-20 20:03:14 +0800

    include crawlers

--- setup.py
+++ setup.py
@@ -8,15 +8,28 @@ base = "Win32GUI" if sys.platform == "win32" else None
 
 proj_root = os.path.abspath(os.path.dirname(__file__))
 
+
 include_files: List[Tuple[str, str]] = [
-        (f"{proj_root}/javspn/core/config.ini", 'config.ini'),
-        (f"{proj_root}/data", 'data'),
-        (f"{proj_root}/image", 'image')
+    (f"{proj_root}/javspn/core/config.ini", 'config.ini'),
+    (f"{proj_root}/data", 'data'),
+    (f"{proj_root}/image", 'image')
 ]
+
+includes = []
+
+for file in os.listdir('javspn/web'):
+    name, ext = os.path.splitext(file)
+    if ext == '.py':
+        includes.append('javspn.web.' + name)
+
+build_exe = {
+    'include_files': include_files,
+    "includes": includes,
+}
 
 setup(
     name='JavSPn',
-    options = {'build_exe': {'include_files': include_files}}, 
+    options = {'build_exe': build_exe}, 
     executables=[Executable("./javspn/__main__.py", target_name='JavSPn', base=base)]
 )
 
